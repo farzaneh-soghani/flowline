d6b9dfb4ec13eb707351089649fd121a562778ac
Farzaneh Soghani <farzanehsoghani@gmail.com>
2026-08-20 02:03:47 +0200

    fix: resolve conflict using revert

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import os
-<<<<<<< HEAD
 from dotenv import load_dotenv
-=======
 import sys
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from datetime import timedelta, datetime
->>>>>>> main
 
 # Lade Umgebungsvariablen aus der .env-Datei
 load_dotenv()
@@ -49,20 +46,6 @@ from reportlab.lib import colors
 
 # Initialisierung der Flask-Anwendung
 app = Flask(__name__)
-<<<<<<< HEAD
-=======
-
-ENV = os.getenv('FLASK_ENV', 'development')
-if ENV == 'production':
-    app.secret_key = os.getenv('SECRET_KEY')
-    if not app.secret_key:
-        sys.exit("ERROR: SECRET_KEY must be set in production!")
-else:
-    app.secret_key = os.getenv('SECRET_KEY') or "local-dev-key"
-    
-app.permanent_session_lifetime = timedelta(days=365)
->>>>>>> main
-
 # --------------------------------------------------------------------------
 # Unterstützte Sprachen (Localization / i18n)
 # - Definition der verfügbaren Sprachen mit Anzeigenamen und Länderkürzel für Flaggen
